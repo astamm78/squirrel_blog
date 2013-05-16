952466ee5c99f1c8a856b34016c9b20e6603d9a1
Andrew Stamm <andrewstamm@me.com>
2013-05-16 18:48:00 -0500

    cleaned up find by tag

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -1,6 +1,6 @@
 get '/tag/:tag_name' do
   @current_tag = params[:tag_name]
-  @all_entries = Post.includes(:entries => :tag).where("tags.tag = ?", params[:tag_name])
+  @all_entries = Tag.find_by_tag(params[:tag_name]).posts
 
   erb :by_tag
 
